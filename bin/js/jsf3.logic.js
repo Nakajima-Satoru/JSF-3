51c22f6bc9808ca3617e6bef756c28a0a06729d8
nakatsuji masato <nakatsuji@teastalk.jp>
2021-04-03 09:39:23 +0900

    4/3

--- bin/js/jsf3.logic.js
+++ bin/js/jsf3.logic.js
@@ -2,7 +2,42 @@ jsf3.logic=function(logicName,settings){
 
     if(settings){
 
+        var colum=Object.keys(settings);
 
+        if(!jsf3.cache.logic[logicName]){
+            jsf3.cache.logic[logicName]={};
+        }
+
+        for(var n=0;n<colum.length;n++){
+            var field=colum[n];
+            var values=settings[field];
+
+            jsf3.cache.logic[logicName][field]=values;
+        }
+
+        return;
     }
 
-}
+    var _this=function(logicName){
+
+        if(!jsf3.cache.logic[logicName]){
+            return;
+        }
+
+        var _logic=jsf3.cache.logic[logicName];
+
+        var _lcolum=Object.keys(_logic);
+
+        for(var n=0;n<_lcolum.length;n++){
+            var methodName=_lcolum[n];
+            var functions=_logic[methodName];
+
+            this[methodName]=functions;
+
+        }
+
+    };
+
+    return new _this(logicName);
+
+};
