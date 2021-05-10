1337dcac37831dd01f6ceda6ec8cecdc94dad701
nakatsuji masato <nakatsuji@teastalk.jp>
2021-05-10 15:42:13 +0900

    5/10

--- template/pc/app/page/login/logout.js
+++ template/pc/app/page/login/logout.js
@@ -6,17 +6,26 @@ jsf3.page("login/logout",{
 
 		jsf3.data("auth").delete();
 
-		$(".sidemenu").removeClass("open").addClass("closed");
-		$("header").removeClass("open").addClass("closed");
-		
-		setTimeout(function(){
-			$(".sidemenu").html("");
-			$("header").html("");
-		},500);
+		$("wk").addClass("closed");
 
-		jsf3.redirect.next("login/index",{
-			bufferClear:true,
-		});
+		setTimeout(function(){
+
+			$("wk").remove();
+
+			$(".sidemenu").removeClass("open").addClass("closed");
+			$("header").removeClass("open").addClass("closed");
+			
+			setTimeout(function(){
+				$(".sidemenu").html("");
+				$("header").html("");
+			},500);
+	
+			jsf3.redirect.next("login/index",{
+				bufferClear:true,
+			});
+	
+		},300);
+		
 	},
 
 });
